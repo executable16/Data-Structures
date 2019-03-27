208200970a5fe4a2ca4ed15aa7a429aa4a41c43d
Atul Malakar <39919653+executable16@users.noreply.github.com>
2019-03-27 14:24:34 +0530

    Create BellManFord.cpp

---
+++ BellManFord.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct graph
+{
+
+	int start;
+	int end;
+	int cost;
+};
+int main()
+{
+	int V,E;
+	cin>>V>>E;
+	graph v[E];
+	int dis[V+1];
+	for(int i=0;i<V+1;i++)
+	{
+		dis[i]=(int)INFINITY;
+	}
+	for(int i=1;i<=E;i++)
+	{
+		cin>>v[i].start;
+		cin>>v[i].end;
+		cin>>v[i].cost;
+	}
+	dis[1]=0;
+	for(int i=0;i<V-1;i++)
+	{
+		int f=0;
+		for(int j=1;j<=E;j++)
+		{
+			if(dis[v[j].start] + v[j].cost < dis[v[j].end])
+			{
+				dis[ v[j].end] = dis[v[j].start] + v[j].cost;
+				f=1;
+			}
+		}
+		if(f==0)
+			break;
+	}
+	for(int i=1;i<=V;i++)
+	{
+		cout<<dis[i]<<endl;
+	}
+}
