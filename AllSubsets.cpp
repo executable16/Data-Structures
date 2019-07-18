8c3bf7be809da984ef048e8e43937eea596149c5
Atul Malakar <atulmalakar15@gmail.com>
2019-07-18 12:53:45 +0530

    Create AllSubsets.cpp

---
+++ AllSubsets.cpp
@@ -0,0 +1,43 @@
+/*
+Huh!
+Tough day right....
+It took so long to really understand this topic finding all subsequences 
+It really needed a lot of effort...
+So let's start------------
+
+Nothing special 
+              ---abc---
+                /  \   
+               /    \
+              {}      a
+             /  \     / \
+            /    \   /   \
+           {}    b   a   ab
+          / \    /\   /\   /\
+         /   \  /  \ /  \ /	 \
+       {}  	 c  b bc a  ac ab abc
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void go(auto &s , auto &temp, int i,int j)
+{
+	if(s[i]=='\0')
+	{	
+		temp[j]='\0';
+		cout<<temp<<" ";
+		return;
+	}
+	temp[j]=s[i];
+	go(s,temp,i+1,j+1); // including
+	go(s,temp,i+1,j);	// excluding
+}
+int main()
+{
+	
+	system("clear");
+	cout<<"Ran successfully\n";
+	char s[10],temp[10];
+	cin>>s;
+	//temp="";
+	go(s,temp,0,0);
+}
